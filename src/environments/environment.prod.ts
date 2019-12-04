940f1030d6a4b7f9d5a0b8683f5a98c9972e2363
Tharuka Kannangara <tharuka.k@eyepax.com>
2019-12-04 12:46:07 +0530

    Release changes to app name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCBuOlO5d-Xe1IJj7kyebTlvFguOAeLCac',
+    authDomain: 'secura-b1bde.firebaseapp.com',
+    databaseURL: 'https://secura-b1bde.firebaseio.com',
+    projectId: 'secura-b1bde',
+    storageBucket: 'secura-b1bde.appspot.com',
+    messagingSenderId: '862866345271'
+  }
 };
