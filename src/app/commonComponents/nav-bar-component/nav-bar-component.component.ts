353bb3a527840dbcfc7336d2db7cdb6ab5b0e519
Tharuka Kannangara <tharuka.k@eyepax.com>
2019-12-04 11:21:04 +0530

    Logout Changes

--- src/app/commonComponents/nav-bar-component/nav-bar-component.component.ts
+++ src/app/commonComponents/nav-bar-component/nav-bar-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'secura-nav-bar-component',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
+
+  logOut(){
+    sessionStorage.removeItem('sessionUrl');
+    sessionStorage.removeItem('sessionUsername');
+    sessionStorage.setItem('loginStatus', '0');
+    this.router.navigate(['']);
+    // this.toastr.success('Logged Out Successfully');
+    console.log("Logged Out Successfully");
+    this.ngOnInit();
+  }
 }
